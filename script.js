86242b97cc05d7d9653fc4536d62c1f1155dbe1e
Perzy-1 <92972869+Perzy-1@users.noreply.github.com>
2023-07-07 02:02:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,6 @@ window.addEventListener('DOMContentLoaded', () => {
     size: 30,
   });
 
-
   
   const italicizeButton = document.getElementById('italicizeButton');
   const italicizeInput = document.getElementById('italicizeInput');
@@ -115,7 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   
 
-
+  
 
 
   function convertToTitleCase(text) {
@@ -188,6 +187,185 @@ window.addEventListener('DOMContentLoaded', () => {
   customButton.addEventListener('click', function () {
     handleTitleCase();
   });
+  const textFileList = document.getElementById("textFileList");
+  
+  const saveButton = document.getElementById("saveButton");
+
+  // Global variables to keep track of the current file and file list
+  let currentFile = null;
+  const fileList = [];
+
+  // Function to rename a file
+  function renameFile(file) {
+    const newName = prompt("Enter new file name:", file.name);
+    if (newName && newName !== file.name) {
+      const existingFile = fileList.find((f) => f.name === newName);
+      if (existingFile) {
+        alert("A file with the same name already exists.");
+        return;
+      }
+
+      const previousName = file.name;
+      file.name = newName;
+      renderFileList();
+
+      // Update the file name in local storage
+      const fileContent = localStorage.getItem(previousName);
+      if (fileContent) {
+        localStorage.removeItem(previousName);
+        localStorage.setItem(newName, fileContent);
+      }
+    }
+  }
+
+
+
+    function renderFileList() {
+      textFileList.innerHTML = "";
+
+      for (const file of fileList) {
+        const fileItem = document.createElement("div");
+        fileItem.classList.add("file-item");
+        if (file === currentFile) {
+          fileItem.classList.add("active");
+        }
+
+        const titleSpan = document.createElement("span");
+        titleSpan.classList.add("file-title");
+        titleSpan.innerText = file.name;
+        titleSpan.addEventListener("click", () => {
+          selectFile(file);
+        });
+
+        const renameIcon = document.createElement("span");
+        renameIcon.classList.add("rename-icon");
+        renameIcon.innerHTML = "&#9998;";
+        renameIcon.addEventListener("click", (event) => {
+          event.stopPropagation();
+          renameFile(file);
+        });
+
+        const deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("delete-icon");
+        deleteIcon.innerHTML = "&#10006;";
+        deleteIcon.addEventListener("click", (event) => {
+          event.stopPropagation();
+          deleteFile(file);
+        });
+
+        
+        textFileList.appendChild(fileItem);
+      
+        fileItem.appendChild(titleSpan);
+        fileItem.appendChild(renameIcon);
+        fileItem.appendChild(deleteIcon);
+      }
+    }
+
+
+
+
+
+
+
+
+
+    function selectFile(file) {
+      if (currentFile) {
+        const fileContent = editor.root.innerHTML;
+        currentFile.content = fileContent;
+        localStorage.setItem(currentFile.name, fileContent);
+      }
+      
+      currentFile = file;
+      renderFileList();
+
+      if (file) {
+        const fileContent = file.content || "";
+        editor.root.innerHTML = fileContent;
+      } else {
+        editor.root.innerHTML = "";
+      }
+    }
+
+    // Function to delete a file
+    function deleteFile(file) {
+      const fileIndex = fileList.findIndex((f) => f === file);
+      if (fileIndex > -1) {
+        fileList.splice(fileIndex, 1);
+        if (file === currentFile) {
+          currentFile = null;
+          editor.root.innerHTML = "";
+        }
+        renderFileList();
+        // Code to delete the file from local storage...
+        localStorage.removeItem(file.name);
+      }
+    }
+
+    // Function to create a new file
+    function createNewFile() {
+      const fileName = prompt("Enter file name:");
+      if (fileName) {
+        const newFile = {
+          name: fileName,
+          content: ""
+        };
+        fileList.push(newFile);
+        selectFile(newFile);
+        saveFile()
+      }
+    }
+
+    // Function to save the current file
+    function saveFile() {
+      if (currentFile) {
+        const fileContent = editor.root.innerHTML;
+        currentFile.content = fileContent;
+        localStorage.setItem(currentFile.name, fileContent);
+        console.log('File saved:', currentFile);
+      }
+    }
+
+    // Function to handle the beforeunload event
+    function handleBeforeUnload(event) {
+      if (currentFile) {
+        const fileContent = editor.root.innerHTML;
+        currentFile.content = fileContent;
+        localStorage.setItem(currentFile.name, fileContent);
+      }
+      event.preventDefault();
+    }
+
+    // Attach the event listener for beforeunload
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    loadFilesFromLocalStorage();
+
+    // Event listeners
+    document.getElementById("newButton").addEventListener("click", createNewFile);
+    saveButton.addEventListener('click', saveFile);
+    // Initialize the file list
+    renderFileList();
+
+
+
+  
+    // Function to load files from local storage
+    function loadFilesFromLocalStorage() {
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const content = localStorage.getItem(key);
+        if (content) {
+          const file = {
+            name: key,
+            content: content
+          };
+          fileList.push(file);
+        }
+      }
+      renderFileList();
+    }
+  
 
   const confettiCanvas = document.getElementById('confetti');
   const confettiContext = confettiCanvas.getContext('2d');
@@ -252,6 +430,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     requestAnimationFrame(animateConfetti);
   }
+
 
   function showMessage(message) {
     const messageElement = document.getElementById('message');
